a0daaa2ea2484dc487a16f948e969310da743542
Chuanqi Zhu <zhu.legend@gmail.com>
2019-06-08 01:01:53 -0700

    Initial function complete

---
+++ countdown-clock/README.markdown
@@ -0,0 +1,4 @@
+# Countdown Clock
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/legendzhuzcq/pen/yWmMvM](https://codepen.io/legendzhuzcq/pen/yWmMvM).
+
+ 
---
+++ countdown-clock/dist/script.js
@@ -0,0 +1,36 @@
+var start_date= new Date().getTime(); // set the countdown date
+var days, hours, minutes, seconds; // variables for time units
+
+var countdown = document.getElementById("tiles"); // get tag element
+
+getCountdown();
+
+setInterval(function () { getCountdown(); }, 1000);
+
+function getCountdown(){
+
+	// find the amount of "seconds" between now and target
+	var current_date = new Date().getTime();
+	var seconds_left = (current_date-start_date ) / 1000;
+
+	days = pad(parseInt(seconds_left / 86400) );
+	seconds_left = seconds_left % 86400;
+		 
+	hours = pad( parseInt(seconds_left / 3600) );
+	seconds_left = seconds_left % 3600;
+		  
+	minutes = pad( parseInt(seconds_left / 60) );
+	seconds = pad( parseInt( seconds_left % 60 ) );
+
+	// format countdown string + set tag value
+	countdown.innerHTML = "<span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>"; 
+}
+
+function pad(n) {
+	return (n < 10 ? '0' : '') + n;
+}
+
+function reset(){
+  start_date= new Date().getTime();
+}
+
